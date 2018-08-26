680494bc5d5cbf5da47269a383c4c845140da4aa
Nikita <topru123@gmail.com>
2018-08-26 04:38:10 +0300

    something happens
    test1

---
+++ JavaScript Main.js
@@ -0,0 +1,28 @@
+$(window).scroll(function(){
+    var scrollval = $(this).scrollTop();    // It will return scroll value
+
+    $("#logo").css("transform",'translate(0px,-'+scrollval/2+'%)');
+    $("#btn").css("transform",'translate(-50%,'+scrollval/3+'%)');
+});
+
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+
+$("#btn").click(function(){
+  player.playVideo();
+});
