5e525c3fefe10ad3d5b808fa34ec810b57811bfe
Nikita <topru123@gmail.com>
2018-08-26 15:17:59 +0300

    Added new logic with fade. New thumbnail.

--- JavaScript Main.js
+++ JavaScript Main.js
@@ -2,33 +2,51 @@ $(window).scroll(function(){
     var scrollval = $(this).scrollTop();    // It will return scroll value
 
     $("#konata").css("transform",'translate(0px,-'+scrollval/2+'%)');
-    $("#btn").css("transform",'translate(-50%,'+scrollval/3+'%)');
+    $("#play_btn").css("transform",'translate(-50%,'+scrollval/3+'%)');
 });
 
+var p = document.getElementById ("video");
+$(p).hide();
+
+var t = document.getElementById ("thumbnail");
+t.src = "http://img.youtube.com/vi/W2TE0DjdNqI/0.jpg";
+
+
 var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
+tag.src = "//www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 
-function onYouTubePlayerAPIReady() {
-  // create the global player from the specific iframe (#video)
-  player = new YT.Player('video', {
-    events: {
-      // call this function when player is ready to use
-      'onReady': onPlayerReady
+onYouTubeIframeAPIReady = function () {
+    player = new YT.Player('video', {
+        height: '100%',
+        width: '100%',
+        videoId: 'W2TE0DjdNqI',  // youtube video id
+        playerVars: {
+            'autoplay': 0,
+            'rel': 0,
+            'showinfo': 0,
+            'modestbranding': 1,
+            'autohide': 1,
+            'controls': 0
+        },
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+onPlayerStateChange = function (event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        $('#play_btn').fadeIn('normal');
     }
-  });
 }
 
-
-function onPlayerReady(event) {
-
-  // bind events
-  var playButton = document.getElementsByClassName("waifu");
-  playButton.addEventListener("click", function() {
+$(document).on('click', '#play_btn', function () {
+    $(this).hide();
+    $("#video").show();
+    $("#thumbnail_container").hide();
     player.playVideo();
-  });
-
-}
+});
