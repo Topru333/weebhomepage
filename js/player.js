7a35a605cf9560b549383bd58e424d66eab01fe1
Nikita <topru123@gmail.com>
2018-08-28 00:46:45 +0300

    Reworked player class

--- js/player.js
+++ js/player.js
@@ -1,87 +1,116 @@
-var tag = document.createElement('script');
+let tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+let firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
+class Player {
+    constructor() {
+        $('#pause_img').hide();
+        $('#next_btn').hide();
+        document.getElementById("player_title").innerText = "";
+        this.iframe_player = new YT.Player('player', {
+            height: '100%',
+            width: '100%',
+            playerVars: {
+                'autoplay': 0,
+                'rel': 0,
+                'showinfo': 0,
+                'modestbranding': 1,
+                'autohide': 1,
+                'controls': 0,
+                'listType': 'playlist',
+                'list': 'PLZqI45FsecAh9WOKC0LTnyFLd3UjyLiE0'
+            },
+            suggestedQuality: 'large',
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+        this.playing = false;
+        this.ranNumber = -1;
+    }
+
+    stateEvent(event) {
+        switch (event.data) {
+            case YT.PlayerState.ENDED:
+                this.iframe_player.playVideoAt(this.newRandomNumber(this.iframe_player.getPlaylist().length));
+                $("#player_title").hide();
+                break;
+            case YT.PlayerState.PLAYING:
+                $("#player_title").show();
+                break;
+            case  YT.PlayerState.PAUSED:
+                $("#player_title").hide();
+                break;
+            default:
+                break;
+        }
+        this.updateTitle();
+    }
+
+    updateTitle() {
+        let title = this.iframe_player.getVideoData().title;
+        if (title.length > 40) {
+            title = title.substr(0, 21);
+        }
+        document.getElementById("player_title").innerText = title;
+    }
+
+    newRandomNumber(max) {
+        let oldNumber = this.ranNumber;
+        this.ranNumber = Math.floor(Math.random() * max);
+        if (this.ranNumber == oldNumber) {
+            return this.newRandomNumber(max);
+        } else {
+            return this.ranNumber;
+        }
+    }
+
+    play() {
+        if (this.playing) {
+            this.playing = false;
+            this.iframe_player.pauseVideo();
+            $('#play_img').show();
+            $('#next_btn').hide();
+            $('#pause_img').hide();
+        } else {
+            this.playing = true;
+            if (this.ranNumber == -1) {
+                this.iframe_player.playVideoAt(this.newRandomNumber(this.iframe_player.getPlaylist().length));
+            } else {
+                this.iframe_player.playVideo();
+            }
+            $('#play_img').hide();
+            $('#next_btn').show();
+            $('#pause_img').show();
+        }
+    }
+
+    next() {
+        if (this.playing) {
+            this.iframe_player.playVideoAt(this.newRandomNumber(this.iframe_player.getPlaylist().length));
+        }
+    }
+}
+
+
+let player_class;
 
 onYouTubeIframeAPIReady = function () {
-    $('#pause_img').hide();
-    $('#next_btn').hide();
-    document.getElementById( "player_title" ).innerText = "";
-    player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        playerVars: {
-            'autoplay': 0,
-            'rel': 0,
-            'showinfo': 0,
-            'modestbranding': 1,
-            'autohide': 1,
-            'controls': 0,
-            'listType': 'playlist',
-            'list': 'PLZqI45FsecAh9WOKC0LTnyFLd3UjyLiE0'
-        },
-        suggestedQuality: 'large',
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
-    });
-
+    player_class = new Player();
 }
 
 onPlayerStateChange = function (event) {
-    if (event.data == YT.PlayerState.ENDED) {
-        player.playVideoAt(newRandomNumber(player.getPlaylist().length));
-    }
-    let title = player.getVideoData().title;
-    if(title.length>40){
-        title = title.substr(0,21);
-    }
-    document.getElementById( "player_title" ).innerText = title;
+    player_class.stateEvent(event);
 }
 
-var playing = false;
-
-
-var ranNumber = -1;
-
-function newRandomNumber(max) {
-    let oldNumber = ranNumber;
-    ranNumber = Math.floor(Math.random() * max);
-    if (ranNumber == oldNumber) {
-        newRandomNumber();
-    } else {
-        return ranNumber;
-    }
-}
 
 $(document).on('click', '.play_button', function () {
-    if (playing) {
-        playing = false;
-        player.pauseVideo();
-        $('#play_img').show();
-        $('#next_btn').hide();
-        $('#pause_img').hide();
-        $("#player_title").hide();
-    } else {
-        playing = true;
-        if(ranNumber==-1){
-            player.playVideoAt(newRandomNumber(player.getPlaylist().length));
-        }else{
-            player.playVideo();
-        }
-        $("#player_title").show();
-        $('#play_img').hide();
-        $('#next_btn').show();
-        $('#pause_img').show();
-    }
+    player_class.play();
 });
 
 $(document).on('click', '.next_button', function () {
-    if (playing) {
-        player.playVideoAt(newRandomNumber(player.getPlaylist().length));
-    }
+    player_class.next();
 });
 
 
--- js/scrolls.js
+++ js/scrolls.js
@@ -1,13 +1,13 @@
 $(window).scroll(function () {
-    var scrollval = $(this).scrollTop();    // It will return scroll value
+    let scrollval = $(this).scrollTop();    // It will return scroll value
     $("#menu").css("opacity",scrollval/410)
+    $("#next_btn").css("opacity",scrollval/420)
     $("#waifu").css("transform", 'translateY(-' + scrollval / 2.6 + '%)');
     $("#play_btn").css("transform", 'translate(' + (toScreenSizeX(scrollval * 3.45) - 50) + '%,' + ((scrollval * 3.2) - 50) + '%)');
     $("#next_btn").css("transform", 'translate(' + (toScreenSizeX(scrollval * 3.45) - 50) + '%,' + ((scrollval * 2.8) - 50) + '%)');
-
 });
 
-var toScreenSizeX = function (a) {
-    var result = a * ($(window).width() / 1900);
+let toScreenSizeX = function (a) {
+    let result = a * ($(window).width() / 1900);
     return result;
 }
